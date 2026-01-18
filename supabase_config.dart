8b0d2c5f521596ef149ae347404777b54dfa1e72
Christ Riand Cornelius Simatupang <jmsesnkiles2375@gmail.com>
2026-01-18 21:50:23 +0700

    Add files via upload

---
+++ supabase_config.dart
@@ -0,0 +1,4 @@
+class SupabaseConfig {
+  static const String supabaseUrl = 'https://jxymeivrrbipgbwnpdtk.supabase.co';
+  static const String supabaseAnonKey = 'sb_publishable_Zyodl40y6_vrBCUpcL0p9Q__vaLBT4W';
+}
